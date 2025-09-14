bcd5b2a18ca5947ba56c45805b8c30c656057483
Thanutham Tuktian <Thanutham.Tn@gmail.com>
2025-09-14 13:28:29 +0700

    feat: :sparkles: integrate flutter_native_splash to wait for critical initialize service

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:unisphere/core/services/key_value_storage_service.dart';
-
 
 final getIt = GetIt.instance;
 
@@ -9,9 +9,10 @@ final getIt = GetIt.instance;
 Future<void> initCriticalServices() async {
   // Initialize local storage service
   await registerLocalStorageDI(getIt);
+  
+  
+  FlutterNativeSplash.remove();
 }
 
 /// Non-critical services initialization
-Future<void> initNonCriticalServices() async {
-
-}
+Future<void> initNonCriticalServices() async {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:unisphere/config/routes/app_router.dart';
 import 'package:unisphere/config/themes/app_theme.dart';
@@ -9,7 +10,8 @@ import 'package:unisphere/injector.dart' as di;
 import 'package:unisphere/l10n/app_localizations.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await di.initCriticalServices();
 

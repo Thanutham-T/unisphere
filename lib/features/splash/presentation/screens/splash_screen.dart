5ecc1055ad4bd397663b2ccea6df86d458e99a3f
Thanutham Tuktian <Thanutham.Tn@gmail.com>
2025-09-14 18:27:29 +0700

    fix: :bug: Re-encode the video & Edit SplashMaster widget option

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:splash_master/splash_master.dart';
-import 'package:unisphere/config/routes/app_router.dart';
-import 'package:unisphere/gen/assets.gen.dart';
 
-import 'package:unisphere/core/cubits/fullscreen_cubit.dart'; // adjust import path
+import 'package:unisphere/config/routes/app_router.dart';
+import 'package:unisphere/core/cubits/fullscreen_cubit.dart';
+
+import 'package:unisphere/gen/assets.gen.dart';
 
 
 class SplashScreen extends StatelessWidget {
@@ -16,6 +17,8 @@ class SplashScreen extends StatelessWidget {
 
     return SplashMaster.video(
       source: AssetSource(Assets.animations.splashAnimation),
+      backGroundColor: Colors.white,
+      videoConfig: VideoConfig(videoVisibilityEnum: VisibilityEnum.none),
       customNavigation: () {
         context.read<FullscreenCubit>().exitFullscreen();
         DashboardRoute().go(context);

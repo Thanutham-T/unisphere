be024dd1cd5b391099d433a737da113b3bab0456
Thanutham Tuktian <Thanutham.Tn@gmail.com>
2025-09-23 13:33:36 +0700

    feat: :sparkles: Add localization setting & Refactor theme management

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ import 'package:splash_master/core/splash_master.dart';
 import 'package:unisphere/config/routes/app_router.dart';
 import 'package:unisphere/config/themes/app_theme.dart';
 import 'package:unisphere/core/services/key_value_storage_service.dart';
-import 'package:unisphere/core/cubits/theme_cubit.dart';
+import 'package:unisphere/core/cubits/app_settings_cubit.dart';
+import 'package:unisphere/core/cubits/app_settings_state.dart';
 import 'package:unisphere/injector.dart' as di;
 import 'package:unisphere/l10n/app_localizations.dart';
 
@@ -17,7 +18,15 @@ void main() async {
 
   await di.initCriticalServices();
   SplashMaster.initialize();
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+      providers: [
+        RepositoryProvider<KeyValueStorageService>.value(
+          value: di.getIt<KeyValueStorageService>(),
+        ),
+      ],
+      child: const MyApp()
+    )
+  );
 
   Future.microtask(() => di.initNonCriticalServices());
 }
@@ -27,33 +36,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(
-      providers: [
-        RepositoryProvider<KeyValueStorageService>.value(
-          value: di.getIt<KeyValueStorageService>(),
-        ),
-      ],
-      child: BlocProvider<ThemeCubit>(
-        create: (_) => ThemeCubit(di.getIt.get<KeyValueStorageService>()),
-        child: BlocBuilder<ThemeCubit, ThemeMode>(
-          builder: (context, state) {
-            return MaterialApp.router(
-              title: 'Unisphere',
-              themeMode: state,
-              theme: AppTheme.lightTheme,
-              darkTheme: AppTheme.darkTheme,
-              routerConfig: appRouter,
-              localizationsDelegates: [
-                AppLocalizations.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              supportedLocales: const [Locale('th'), Locale('en')],
-              locale: const Locale('en'),
-            );
-          },
-        ),
+    return BlocProvider<AppSettingsCubit>(
+      create: (_) => AppSettingsCubit(di.getIt.get<KeyValueStorageService>()),
+      child: BlocBuilder<AppSettingsCubit, AppSettingsState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            title: 'Unisphere',
+            themeMode: state.themeMode,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            routerConfig: appRouter,
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [Locale('th', 'TH'), Locale('en', 'US')],
+            locale: state.locale,
+            localeResolutionCallback: (locale, supportedLocales) {
+              if (locale != null && supportedLocales.contains(locale)) {
+                return locale;
+              }
+              return state.locale ?? const Locale('en');
+            },
+          );
+        },
       ),
     );
   }

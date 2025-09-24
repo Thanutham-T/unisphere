b39a2c96092407f4361e82bbda72e8c024c851ec
Thanutham Tuktian <Thanutham.Tn@gmail.com>
2025-09-24 12:25:30 +0700

    style: :lipstick: Update app_theme color

---
+++ lib/config/themes/app_color.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  static const Color primary = Color.fromARGB(255, 3, 60, 159);
+  static const Color secondary = Color.fromARGB(255, 68, 137, 255);
+
+  static const Color backgroundLight = Color.fromARGB(255, 249, 249, 249);
+  static const Color surfaceLight = Colors.white;
+
+  static const Color backgroundDark = Color.fromARGB(255, 18, 18, 18);
+  static const Color surfaceDark = Color.fromARGB(255, 30, 30, 30);
+
+  static const Color textDark = Color.fromARGB(255, 33, 33, 33);
+  static const Color textLight = Colors.white;
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
